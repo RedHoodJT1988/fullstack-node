a52bb6c03e5232b81f5dbd48f4098614077107a1
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-07-06 13:23:08 -0500

    added express

--- Basic-Server/server.js
+++ Basic-Server/server.js
@@ -1,10 +1,13 @@
 // import statements
 const fs = require('fs');
-const http = require('http');
-const querystring = require('querystring');
+
+const express = require('express');
 
 // set the port variable to use the built in env port or default to 1666
 const port = process.env.PORT || 1666;
+
+// initialize our app using express
+const app = express();
 
 // plain text response
 function respondText (req, res) {
@@ -14,8 +17,7 @@ function respondText (req, res) {
 
 // json response using ES6 arrow function syntax
 const respondJson = (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ text: 'Riddle me this Batman.', luckyNumbers: [42, 3, 27, 13]}));
+    res.json({ text: 'Riddle me this Batman.', luckyNumbers: [42, 3, 27, 13]});
 }
 
 // not found error response
@@ -26,44 +28,31 @@ const respondNotFound = (req, res) => {
 
 // print out a query param
 const respondEcho = (req, res) => {
-    const { input = '' } = querystring.parse(
-        req.url
-            .split('?')
-            .slice(1)
-            .join('')
-    )
+    const { input = '' } = req.query;
 
-    res.setHeader('Content-Type', 'application/json')
-    res.end(
-        JSON.stringify({
-            normal: input,
-            shouty: input.toUpperCase(),
-            characterCount: input.length,
-            backwards: input
-                .split('')
-                .reverse()
-                .join('')
-        })
-    )
+    res.json({
+        normal: input,
+        shouty: input.toUpperCase(),
+        characterCount: input.length,
+        backwards: input
+            .split('')
+            .reverse()
+            .join('')
+    })
 }
 
 const respondStatic = (req, res) => {
-    const filename = `${__dirname}/public${req.url.split('/static')[1]}`;
+    const filename = `${__dirname}/public/${req.params[0]}`;
     fs.createReadStream(filename)
         .on('error', () => respondNotFound(req, res))
         .pipe(res)
 }
 
-// create the server and respond with a message
-const server = http.createServer(function (req, res) {
-    if (req.url === '/') return respondText(req, res);
-    if (req.url === '/json') return respondJson(req, res);
-    if (req.url.match(/^\/echo/)) return respondEcho(req, res);
-    if (req.url.match(/^\/static/)) return respondStatic(req, res)
-
-    respondNotFound(req, res);
-});
+// add the routes for our API
+app.get('/', respondText);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/static/*', respondStatic);
 
 // set the server to listen on the given port
-server.listen(port);
-console.log(`Server listening on port ${port}...`);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
