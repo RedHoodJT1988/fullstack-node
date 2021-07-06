f349debc695c6e33b8bd0d39ec9cea2468c6259e
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-07-06 12:52:16 -0500

    dynamic routing added

--- ch01/server.js
+++ ch01/server.js
@@ -1,5 +1,6 @@
 // import statements
 const http = require('http');
+const querystring = require('querystring');
 
 // set the port variable to use the built in env port or default to 1666
 const port = process.env.PORT || 1666;
@@ -22,10 +23,34 @@ const respondNotFound = (req, res) => {
     res.end('Not Found');
 }
 
+// print out a query param
+const respondEcho = (req, res) => {
+    const { input = '' } = querystring.parse(
+        req.url
+            .split('?')
+            .slice(1)
+            .join('')
+    )
+
+    res.setHeader('Content-Type', 'application/json')
+    res.end(
+        JSON.stringify({
+            normal: input,
+            shouty: input.toUpperCase(),
+            characterCount: input.length,
+            backwards: input
+                .split('')
+                .reverse()
+                .join('')
+        })
+    )
+}
+
 // create the server and respond with a message
 const server = http.createServer(function (req, res) {
     if (req.url === '/') return respondText(req, res);
     if (req.url === '/json') return respondJson(req, res);
+    if (req.url.match(/^\/echo/)) return respondEcho(req, res);
 
     respondNotFound(req, res);
 });
